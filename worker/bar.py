a615d85efeed01ffacb08f099e02e404f5f9a27f
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-09-27 15:20:41 -0300

    Adding travis rubygems config

---
+++ worker/bar.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestFoo(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(True)
+
---
+++ worker/foo.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestFoo(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(False)
