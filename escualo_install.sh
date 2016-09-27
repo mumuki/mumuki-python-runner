ebce3dd8943b1d4af2375ba1885225b0e04385c3
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-09-27 19:28:45 -0300

    Removing escualo config

--- escualo_install.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-REV=$1
-
-echo "[Escualo::PythonServer] Fetching GIT revision"
-echo -n $REV > version
-
-echo "[Escualo::PythonServer] Pulling docker image"
-docker pull mumuki/mumuki-python-worker
