e961270a60282fb72ed3bab706677257927d212a
Julián Berbel Alt <julianberbel@hotmail.com>
2019-02-06 15:25:18 -0300

    add tests for expected behavior

--- spec/query_runner_spec.rb
+++ spec/query_runner_spec.rb
@@ -37,4 +37,24 @@ describe PythonQueryHook do
     let(:request) { struct query: '4 + x', content: 'x = 10' }
     it { expect(result).to eq ["=> 14\n", :passed] }
   end
+
+  context 'passes when query is an assignment' do
+    let(:request) { struct query: 'foo = 123' }
+    it { expect(result).to eq ["", :passed] }
+  end
+
+  context 'is stateful' do
+    let(:request) { struct query: 'print foo', cookie: ['foo = 123'] }
+    it { expect(result).to eq ["123\n", :passed] }
+  end
+
+  context 'does not redo prints in cookie' do
+    let(:request) { struct query: 'print "foo"', cookie: ['print bar'] }
+    it { expect(result).to eq ["foo\n", :passed] }
+  end
+
+  context 'does not fail if an exception was thrown in cookie' do
+    let(:request) { struct query: 'print "foo"', cookie: ['raise(Exception("bar"))'] }
+    it { expect(result).to eq ["foo\n", :passed] }
+  end
 end
