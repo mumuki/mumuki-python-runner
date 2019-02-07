891e5e75b22514eb2b6039628b7644e61f2293f2
Franco Bulgarelli <franco@mumuki.org>
2019-02-07 11:53:19 -0300

    Marking runner as stateful

--- lib/python_runner.rb
+++ lib/python_runner.rb
@@ -6,6 +6,7 @@ I18n.load_translations_path File.join(__dir__, 'locales', '*.yml')
 Mumukit.runner_name = 'python'
 Mumukit.configure do |config|
   config.docker_image = 'mumuki/mumuki-python-worker:1.1'
+  config.stateful = true
 end
 
 require_relative 'version'
