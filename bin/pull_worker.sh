c6ac8cf224090460d2df9ad42eca92a1c15abe8b
Franco Bulgarelli <franco@mumuki.org>
2019-04-29 13:07:45 -0300

    Adding set-e to bash scripts

--- bin/pull_worker.sh
+++ bin/pull_worker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 TAG2=$(grep -e 'mumuki/mumuki-python2-worker:[0-9]*\.[0-9]*' ./lib/python2_runner.rb -o | tail -n 1)
 TAG3=$(grep -e 'mumuki/mumuki-python3-worker:[0-9]*\.[0-9]*' ./lib/python3_runner.rb -o | tail -n 1)
