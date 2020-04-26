324c0456b1d8fd835dbe97edf8b072d649996268
Franco Bulgarelli <franco@mumuki.org>
2020-04-26 01:12:05 -0300

    Refactor: extracting compile_header method

--- lib/base/query_hook.rb
+++ lib/base/query_hook.rb
@@ -7,6 +7,10 @@ class BasePythonQueryHook < Mumukit::Templates::FileHook
   end
 
   def compile_file_content(req)
+    "#{compile_file_header(req)}\n#{compile_query(req.query)}"
+  end
+
+  def compile_file_header(req)
     <<python
 # -*- coding: UTF-8 -*-
 import string, sys, os
@@ -16,19 +20,18 @@ import string, sys, os
 sys.stdout = open(os.devnull, 'w')
 #{compile_cookie(req.cookie)}
 sys.stdout = sys.__stdout__
-#{build_query req.query}
 python
   end
 
-  def build_query(query)
+  def compile_query(query, output_prefix = "=> ", output_var = "__mumuki_query_result__")
     if query.match /print *(\(| ).*|^[a-zA-Z_]\w*\s*=.*|^raise\b/
       query
     else
-      "print(string.Template(\"=> $result\").substitute(result = #{query}))"
+      "print(string.Template(\"#{output_prefix}$#{output_var}\").substitute(#{output_var} = #{query}))"
     end
   end
 
-  def build_state(cookie)
+  def compile_state(cookie)
     (cookie||[]).map do |statement|
     <<~python
     try:
@@ -40,7 +43,7 @@ python
   end
 
   def compile_cookie(cookie)
-    build_state(cookie).join("\n")
+    compile_state(cookie).join("\n")
   end
 
   def error_patterns
