10a06675c70709dd091a9008b3c2adcecd8ed78f
Franco Leonardo Bulgarelli <franco@mumuki.org>
2023-02-04 01:27:07 -0300

    Test custom expectations more in deep

--- spec/python3/expectations_hook_spec.rb
+++ spec/python3/expectations_hook_spec.rb
@@ -2,7 +2,8 @@ require_relative './spec_helper'
 
 describe Python3ExpectationsHook do
   let(:runner) { Python3ExpectationsHook.new }
-  let(:result) { runner.run!(runner.compile(struct(expectations: expectations, content: code))) }
+  let(:result) { runner.run!(runner.compile(struct(expectations: expectations, custom_expectations: custom_expectations, content: code))) }
+  let(:custom_expectations) { nil }
 
   it_behaves_like 'common python expectations hook'
 
@@ -89,20 +90,43 @@ describe Python3ExpectationsHook do
       def map_plus_three(xs):
         result = []
         for x in xs:
-          result.append(x + 4)
+          result.append(x + 3)
         return result
+
+      def map_plus_four(xs):
+        for x in xs:
+          yield x + 4
       EOC
     end
     let(:expectations) { [
       {binding: 'map_plus_one', inspection: 'UsesComprehension'},
       {binding: 'map_plus_two', inspection: 'UsesComprehension'},
-      {binding: 'map_plus_four', inspection: 'UsesComprehension'},
+      {binding: 'map_plus_three', inspection: 'UsesComprehension'},
     ] }
 
-    it { expect(result).to eq [
-        {expectation: expectations[0], result: true},
-        {expectation: expectations[1], result: true},
-        {expectation: expectations[2], result: false}] }
+    let(:custom_expectations) do
+      <<~EOC
+      expectation "map_plus_one uses a list comprehension": within `map_plus_one` calls `list` with something that (uses comprehension);
+      expectation "map_plus_two uses a list comprehension": within `map_plus_two` calls `list` with something that (uses comprehension);
+
+      expectation "map_plus_four returns a comprehension": within `map_plus_four` returns with something that (uses comprehension);
+      expectation "map_plus_four uses a comprehension": within `map_plus_four` uses comprehension;
+      EOC
+    end
+
+    it do
+      expect(result.pluck(:result)).to eq [
+        true,
+        true,
+        false,
+
+        false,
+        true,
+
+        false,
+        true
+      ]
+    end
   end
 
 
